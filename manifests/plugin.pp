68398ebde9f851450307b8fbbb21396899749a77
Les Aker <me@lesaker.org>
2017-01-09 21:13:00 -0800

    use tool instead of repo

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,14 +1,32 @@
 # Define plugin type for asdf
 define asdf::plugin (
   Enum['present', 'absent', 'latest'] $ensure = 'latest',
-  String[1] $shortname = $title,
+  Variant[String[1], Undef] $shortname = undef,
   String[1] $repo = $title,
 ) {
-  vcsrepo { "${asdf::path}/plugins/${shortname}":
-    ensure   => $ensure,
-    provider => git,
-    source   => $repo,
-    owner    => $asdf::owner,
-    group    => $asdf::group
+  if $shortname == undef {
+    $shortname = split($repo, '-')[-1]
+  }
+  $path = "${asdf::path}/plugins/${shortname}"
+
+  Exec {
+    cwd   => "${asdf::path}/bin",
+    user  => $asdf::owner,
+    group => $asdf::group
+  }
+
+  if $ensure == 'absent' {
+    exec { "./asdf plugin-remove ${shortname} ${repo}":
+      onlyif => "test -d ${path}"
+    }
+  } else {
+    exec { "./asdf plugin-add ${shortname} ${repo}":
+      creates => $path
+    }
+    if $ensure == 'latest' {
+      exec { "./asdf plugin-update ${shortname}":
+        require => Exec["./asdf plugin-add ${shortname} ${repo}"]
+      }
+    }
   }
 }
