be04642deac37f749476b5824f45a21a5fc5076a
Les Aker <me@lesaker.org>
2017-01-11 22:35:23 -0800

    use repo for plugins

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -9,26 +9,15 @@ define asdf::plugin (
   } else {
     $_shortname = $shortname
   }
+
   $path = "${asdf::path}/plugins/${_shortname}"
-  $bin = "${asdf::path}/bin/asdf"
 
-  Exec {
-    user  => $asdf::owner,
-    group => $asdf::group
-  }
-
-  if $ensure == 'absent' {
-    exec { "${bin} plugin-remove ${_shortname} ${repo}":
-      onlyif => "test -d ${path}"
-    }
-  } else {
-    exec { "${bin} plugin-add ${_shortname} ${repo}":
-      creates => $path
-    }
-    if $ensure == 'latest' {
-      exec { "${bin} plugin-update ${_shortname}":
-        require => Exec["${bin} plugin-add ${_shortname} ${repo}"]
-      }
-    }
+  vcsrepo { $path:
+    ensure   => $ensure,
+    provider => git,
+    source   => $repo,
+    owner    => $asdf::owner,
+    group    => $asdf::group,
+    require  => $asdf::path
   }
 }
